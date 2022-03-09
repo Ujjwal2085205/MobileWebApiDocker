174bb4f61422488360424dc080360027f2b23262
Raj <2085205@cognizant.com>
2022-03-09 22:22:36 +0530

    new

---
+++ MobileDockerAzure/Controllers/MobileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileDockerAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MobileController : ControllerBase
+    {
+        List<Mobile> MobileList = new List<Mobile>()
+        {
+            new Mobile { Name="Samsung S21", Color="White",Price=32000 },
+            new Mobile { Name="Redmi Note 10", Color="Blue",Price=22000 },
+            new Mobile { Name="Lenevo K19+", Color="Glacier",Price=19999 },
+            new Mobile { Name="Apple Iphone 13", Color="Red",Price=75000 },
+
+
+        };
+
+        public List<Mobile> GetMobile()
+        {
+            return MobileList;
+        }
+    }
+}
